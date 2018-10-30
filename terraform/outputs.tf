46bf40d668152a82b4083958f95eb96442f7fbe6
cxhercules <cxhercules@users.noreply.github.com>
2018-10-29 20:43:14 -0700

    Demo code

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -26,3 +26,7 @@ output "client_key" {
 output "cluster_ca_certificate" {
   value = "${google_container_cluster.primary.master_auth.0.cluster_ca_certificate}"
 }
+
+output "endpoint" {
+  value = "${google_container_cluster.primary.endpoint}"
+}
